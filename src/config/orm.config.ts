672c280b7bc729f7215468ef97c8950d1ce1e018
claudio pizarro <claudeus123>
2023-05-25 13:07:44 -0400

    DB Linked

---
+++ src/config/orm.config.ts
@@ -0,0 +1,31 @@
+import { DataSourceOptions } from "typeorm";
+import { ConfigModule } from "@nestjs/config";
+
+ConfigModule.forRoot({
+    envFilePath : ".env",
+    isGlobal : true
+})
+
+const DatabaseConfig: DataSourceOptions = {
+    type: process.env.TYPEORM_CONNECTION as any,
+    host: process.env.TYPEORM_HOST,
+    port: parseInt(process.env.TYPEORM_PORT as string),
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
+    synchronize: process.env.TYPEORM_SYNCHRONIZE === "true",
+    logging: process.env.TYPEORM_LOGGING === "true",
+    entities: [],
+    migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+}
+
+class DataSourceFactory {
+    private constructor(){}
+
+    static createDataSource(): DataSourceOptions{
+        return DatabaseConfig
+    }
+}
+
+const DataSourceConfig = DataSourceFactory.createDataSource();
+export { DataSourceConfig };
